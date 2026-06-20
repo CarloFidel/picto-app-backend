08b9b0b8a73b60c7e032073241859ecbf32878d9
CarloFidel <carloftaboada@gmail.com>
2026-06-20 12:40:40 +0200

    add: ssl in mofule.app and config database in neon

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module';
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT!,
